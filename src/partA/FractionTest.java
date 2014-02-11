a039e88e2b34e6cdeef09a0350ee0df76d64d98e
AbbyStevens <stevensa1@grinnell.edu>
2014-02-10 19:38:35 -0600

    added fraction tests

---
+++ src/partA/FractionTest.java
@@ -0,0 +1,132 @@
+package partA;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FractionTest
+{
+
+  @Test
+  public void
+    testFractionBigIntegerBigInteger ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testFractionBigInteger ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testFractionIntInt ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testFractionInt ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testFractionString ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testFractionLongLong ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testFractionDouble ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testToString ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testCompareTo ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testEqualsFraction ()
+  throws Exception
+  {
+    assertEquals(true, new Fraction(1,3).equals(new Fraction(2,6)));
+  }
+
+  @Test
+  public void
+    testEqualsObject ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testAdd ()
+    throws Exception
+  {
+assertEquals(new Fraction(2,3),new Fraction(1,3).add(new Fraction(1,3)));
+    
+  }
+
+  @Test
+  public void
+    testToDouble ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testToBigDecimal ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testMultiplyBy ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testDivideBy ()
+  {
+    fail ("Not yet implemented");
+  }
+
+  @Test
+  public void
+    testNegate ()
+  {
+    fail ("Not yet implemented");
+  }
+
+}
